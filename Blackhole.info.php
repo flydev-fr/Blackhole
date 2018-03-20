a774c7151a362bd2e914f9e8b2101b41f62cd21e
flydev-fr <flydev@sekretservices.com>
2018-03-20 12:17:48 +0100

    update version info

--- Blackhole.info.php
+++ Blackhole.info.php
@@ -3,7 +3,7 @@
 $info = array(
 	'title' => 'Blackhole',
 	'summary' => 'Trap bad bots, crawlers and spiders in a virtual black hole.',
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'author' => 'flydev',
 	'icon' => 'grav',
 	'href' => 'https://github.com/flydev-fr/Blackhole',
